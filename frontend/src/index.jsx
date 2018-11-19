5703778523a24a16ecb2505dfba51476cc0ad60a
Aarni Halinen <aarni.halinen@aalto.fi>
2018-11-20 00:00:57 +0200

    Show multiple icons

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,17 +1,44 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+// const navigator = require('navigator');
+
 const baseURL = process.env.ENDPOINT;
+const defaultLocation = 'Helsinki,fi';
 
-const getWeatherFromApi = async () => {
-  try {
-    const response = await fetch(`${baseURL}/weather`);
-    return response.json();
-  } catch (error) {
-    console.error(error);
-  }
+// function geoSuccess(position) {
+//   const { latitude, longitude } = position.coords;
+//   return [latitude, longitude];
+// }
 
-  return {};
+// function geoError() {
+//   window.alert('Unable to retrieve your location');
+// }
+
+// const geoOptions = {
+//   enableHighAccuracy: true,
+//   maximumAge: 30000,
+//   timeout: 27000,
+// };
+
+// const getLocation = () => {
+//   console.log("test");
+//   if (!navigator.geolocation) {
+//     window.alert('Geolocation is not supported by your browser');
+//     return;
+//   }
+//   navigator.geolocation.getCurrentPosition(geoSuccess, geoError, geoOptions);
+
+  // try {
+  //   if ('geolocation' in navigator && navigator.geolocation !== undefined) {
+  //     navigator.geolocation.getCurrentPosition(geoSuccess, geoError, geoOptions);
+  //   }
+  // } catch (err) { return defaultLocation; }
+// };
+
+const getWeatherFromApi = async (location) => {
+  const response = await fetch(`${baseURL}/weather?n=5&location=`.concat(location));
+  return response.json();
 };
 
 class Weather extends React.Component {
@@ -19,21 +46,30 @@ class Weather extends React.Component {
     super(props);
 
     this.state = {
-      icon: '',
+      icons: [],
     };
   }
 
   async componentWillMount() {
-    const weather = await getWeatherFromApi();
-    this.setState({ icon: weather.icon.slice(0, -1) });
+    // const location = await getLocation();
+    // const weather = await getWeatherFromApi(location);
+    const weather = await getWeatherFromApi(defaultLocation);
+    console.log(weather);
+
+    const wIcons = weather.map(forecast => forecast[0].icon.slice(0, -1));
+    this.setState({ icons: wIcons });
   }
 
   render() {
-    const { icon } = this.state;
-
+    const { icons } = this.state;
     return (
-      <div className="icon">
-        { icon && <img src={`/img/${icon}.svg`} alt="" /> }
+      <div className="icons">
+        {/* <p><button onClick="getLocation()">Show my location</button></p> */}
+        {icons.map(icon => (
+          <div className="icon">
+            <img src={`/img/${icon}.svg`} alt="" />
+          </div>
+        ))}
       </div>
     );
   }
